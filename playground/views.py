fa04296603b3494284966f8571d507ecdbd05767
Jean-Yves TRAN <jendives2000@gmail.com>
2024-03-20 22:21:38 +0200

    Changed def hello (render hello.html)

--- playground/views.py
+++ playground/views.py
@@ -1,4 +1,5 @@
 # imported by default
+# render actually renders the HTML templates
 from django.shortcuts import render
 
 # imported for this project:
@@ -14,4 +15,7 @@ def hello(request):
     # and a lot more
 
     # output an instance of the response when responding to the request (HTTP protocol)
-    return HttpResponse("Hello, world. This is an HTTP Response.")
+    # return HttpResponse("Hello, world. This is an HTTP Response.")
+
+    # renders the HTML template that is passed as an argument
+    return render(request, "hello.html")
