7baa24ac9b1b1250add79518195d4542b551a3c0
Jean-Yves TRAN <jendives2000@gmail.com>
2024-03-21 11:14:20 +0200

    Added class Product

--- store/models.py
+++ store/models.py
@@ -1,3 +1,15 @@
+# for a list of models filed types:
+# https://docs.djangoproject.com/en/5.0/ref/models/fields/#field-types
+
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+    last_updated = models.DateTimeField(auto_now=True)
+
+
+class 
