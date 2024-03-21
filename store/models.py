7ccc184b4cfa876edb6f2a7579e0ea22d26d7a1d
Jean-Yves TRAN <jendives2000@gmail.com>
2024-03-21 15:00:35 +0200

    Added model named Address (with a OnetoOne attriubute)

--- store/models.py
+++ store/models.py
@@ -46,3 +46,11 @@ class Order(models.Model):
     payment_status = models.CharField(
         max_length=1, choices=PAYMENT_STATUSES, default=PAYMENT_STATUS_PENDING
     )
+
+
+class address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.OneToOneField(
+        Customer, on_delete=models.CASCADE, primary_key=True
+    )
