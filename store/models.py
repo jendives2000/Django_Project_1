43fc56e6e852ed96a2dfa017d74738639f3e436f
Jean-Yves TRAN <jendives2000@gmail.com>
2024-03-21 14:43:08 +0200

    Added class named Customer

--- store/models.py
+++ store/models.py
@@ -12,4 +12,21 @@ class Product(models.Model):
     last_updated = models.DateTimeField(auto_now=True)
 
 
-class 
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = "B"
+    MEMBERSHIP_SILVER = "S"
+    MEMBERSHIP_GOLD = "G"
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, "Bronze"),
+        ("MEMBERSHIP_SILVER", "Silver"),
+        ("MEMBERSHIP_GOLD", "Gold"),
+    ]
+    first_name = models.CharField(max_length=55)
+    last_name = models.CharField(max_length=55)
+    email = models.EmailField(unique=True, max_length=155)
+    phone = models.CharField(max_length=12)
+    birth_date = models.DateField(null=True, blank=True)
+    membership = models.CharField(
+        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE
+    )
