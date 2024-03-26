7032779f72c2e45d8447c932760cf68f030eb7fe
Jean-Yves TRAN <jendives2000@gmail.com>
2024-03-26 11:23:59 +0200

    Defined classes Tag & TaggedItems for generic relationships creation

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,26 @@
-from django.db import models
+# the app of this model (tags) is meant to be usable with any other app. So it is independent.
+# in other words, it creates generic relationships.
+# This is why we use the default app contenttype (#1).
 
-# Create your models here.
+# NOTE:
+# This will NOT work with GUID type ID because they are not integers.
+
+from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    # get what tag is applied to what object
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    # We can identify any object from any app (record from any DB) with these 2: Type, ID
+    # Type: could be anything: product, video, article, etc.
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)  # 1
+    # ID:
+    object_id = models.PositiveIntegerField()
+    # get the actual product or object name
+    actual_object = GenericForeignKey()
