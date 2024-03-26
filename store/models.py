aa530d6fa91e32adf739415230c0170faf4d50f8
Jean-Yves TRAN <jendives2000@gmail.com>
2024-03-26 16:16:05 +0200

    makemigrations

--- store/models.py
+++ store/models.py
@@ -28,7 +28,7 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
 
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
     last_updated = models.DateTimeField(auto_now=True)
     # !! DEPENDENCY -- 1 -- : Mto1 !!:
